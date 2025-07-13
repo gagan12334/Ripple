b5fe85af3b621149f94e7ead248d999b0fa38b11
Gagan Charagondla <gagancharagondla@gagans-mbp.mynetworksettings.com>
2025-07-13 01:01:19 -0400

    added classes for nodes for ast

---
+++ parser.py
@@ -0,0 +1,5 @@
+class NumberNode:
+  def __init__(self, num):
+    self.num = num
+  def __repr__(self):
+    pass
--- shell.py
+++ shell.py
@@ -1,3 +1,33 @@
-while True:
-  expression = input("ripple> ")
-  print(expression)
+#########################
+# NODE CLASSES
+#########################
+class NumberNode:
+  def __init__(self, tok):
+    self.tok = tok
+  def __repr__(self):
+    return f'{self.tok}'
+
+class BinOpNode:
+  def __init__(self, left_tok, op_tok ,right_tok):
+    self.left_tok = left_tok
+    self.op_tok = op_tok
+    self.right_tok = right_tok
+  def __repr__(self):
+    return f'{self.left_tok}, {self.op_tok}, {self.right_tok}'
+
+class UnOpNode: # for stuff like (-5) 
+  def __init__(self, op_tok, tok):
+    self.op_tok = op_tok
+    self.tok = tok
+  def __repr__(self):
+    return f'{self.op_tok}, {self.tok}'
+
+#ParenNode is for  partenthesized expressesions like (2+3)
+class ParenNode:
+  def __init__(self, node):
+    self.node = node
+  def __repr__(self):
+    return f'{self.node}'
+  
+
+
